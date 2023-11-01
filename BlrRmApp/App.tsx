276e8be8a93da633477bddc06af6aa086d806a94
Misael <maq3009@hotmail.com>
2023-11-01 01:30:21 -0400

    second commit added Stack Navigation List and Details page

---
+++ BlrRmApp/App.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import List from './app/screens/List';
+import Details from './app/screens/Details'
+
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="My Todos" component={List}/>
+        <Stack.Screen name="Details" component={Details}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )}
+
---
+++ BlrRmApp/firebaseConfig.tsx
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAXEiFKMvRXjhSnT1ceawbEn2vVhxvRmk8",
+    authDomain: "boilerinventory.firebaseapp.com",
+    projectId: "boilerinventory",
+    storageBucket: "boilerinventory.appspot.com",
+    messagingSenderId: "626595907158",
+    appId: "1:626595907158:web:bf171964e091741ca45df7",
+    measurementId: "G-NDJMQFYQ80"
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
