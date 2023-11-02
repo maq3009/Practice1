b4bbaa7c88ec47f3e6e76636b9457cc92449c00b
Misael <maq3009@hotmail.com>
2023-11-02 19:41:17 -0400

    Still working- added Inventory PartName etc

--- BlrRmApp/app/screens/Inventory.tsx
+++ BlrRmApp/app/screens/Inventory.tsx
@@ -1,13 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SearchBar } from 'react-native-elements'
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-const Inventory = () => {
+
+
+const Inventory = ({ route }: any) => {
+    const PartName = route.params?.PartName || '';
+    const PartNumber = route.params?.PartNumber || '';
+    const Location = route.params?.Location || '';
+    const Manufacturer = route.params?.Manufacturer || '';
+    const Quantity = route.params?.Quantity || '';
+
+  const data = [
+    { label: 'Part Name', value: PartName },
+    { label: 'Part Number', value: PartNumber },
+    { label: 'Location', value: Location },
+    { label: 'Manufacturer', value: Manufacturer },
+    { label: 'Quantity', value: Quantity },
+  ];
+
   return (
-    <View>
-      <Text>Inventory</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <Text style={styles.PageTitle}>Inventory</Text>
 
-export default Inventory
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.label}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Text style={styles.label}>{item.label}:</Text>
+            <Text style={styles.value}>{item.value}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default Inventory;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  PageTitle: {
+    fontSize: 28,
+    justifyContent: 'center',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  value: {
+    fontSize: 18,
+  },
+});
--- BlrRmApp/app/screens/List.tsx
+++ BlrRmApp/app/screens/List.tsx
@@ -1,98 +1,99 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
-import { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { useState } from 'react';
 import { FIRESTORE_DB } from '../../firebaseConfig';
 import { addDoc, collection } from 'firebase/firestore';
 
-
-
 const List = ({ navigation }: any) => {
-  const [PartNames, setPartNames] = useState<any[]>([]);
   const [PartName, setPartName] = useState('');
-  const [PartNumbers, setPartNumbers] = useState<any[]>([]);
   const [PartNumber, setPartNumber] = useState('');
-  const [Locations, setLocations] = useState<any[]>([]);
   const [Location, setLocation] = useState('');
-  const [Manufacturers, setManufacturers] = useState<any[]>([]);
   const [Manufacturer, setManufacturer] = useState('');
-  const [Quantities, setQuantities] = useState<any[]>([]);
   const [Quantity, setQuantity] = useState('');
-  
-  useEffect(() => {
-  },[]);
 
-  const addPartName = async () => {
-    const doc = await addDoc(collection(FIRESTORE_DB, 'PartNames'), {
-       Part_Name: PartName, 
-       Out_of_Stock: false})
+  const addPart = async () => {
+    const doc = await addDoc(collection(FIRESTORE_DB, 'Inventory'), {
+      PartName,
+      PartNumber,
+      Location,
+      Manufacturer,
+      Quantity,
+      Out_of_Stock: false,
+    });
+
+    // Clear input fields after adding the part
     setPartName('');
-  };
-  const addPartNumber = async () => {
-    const doc = await addDoc(collection(FIRESTORE_DB, 'PartNumbers'), {
-       Part_Number: PartNumber, 
-       Out_of_Stock: false})
     setPartNumber('');
-  };
-  const addLocation = async () => {
-    const doc = await addDoc(collection(FIRESTORE_DB, 'Locations'), {
-       Part_Location: Location, 
-       Out_of_Stock: false})
     setLocation('');
-  };
-  const addManufacturer = async () => {
-    const doc = await addDoc(collection(FIRESTORE_DB, 'Manufacturers'), {
-       Part_Manufacturer: Manufacturer, 
-       Out_of_Stock: false})
     setManufacturer('');
-  };
-  const addQuantity = async () => {
-    const doc = await addDoc(collection(FIRESTORE_DB, 'Quantities'), {
-       Part_Quantity: Quantity, 
-       Out_of_Stock: false})
     setQuantity('');
   };
-
 
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.TopText}>ADD PART</Text>
-        <TextInput style={styles.input} placeholder='Part Name'
-        onChangeText={(text: string) => setPartName(text)}
-        value={PartName}/>
-        <TextInput style={styles.input} placeholder='Part Number'
-        onChangeText={(text: string) => setPartNumber(text)}
-        value={PartNumber}/>
-        <TextInput style={styles.input} placeholder='Location'
-        onChangeText={(text: string) => setLocation(text)}
-        value={Location}/>
-        <TextInput style={styles.input} placeholder='Manufacturer'
-        onChangeText={(text: string) => setManufacturer(text)}
-        value={Manufacturer}/>
-        <TextInput style={styles.input} placeholder='Quantity'
-        onChangeText={(text: string) => setQuantity(text)}
-        value={Quantity}/>
-        <Button onPress={() => [addPartName(), addPartNumber(), addLocation(), addManufacturer(), addQuantity()]} title="Add Part" />
+        <TextInput
+          style={styles.input}
+          placeholder="Part Name"
+          onChangeText={(text: string) => setPartName(text)}
+          value={PartName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Part Number"
+          onChangeText={(text: string) => setPartNumber(text)}
+          value={PartNumber}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Location"
+          onChangeText={(text: string) => setLocation(text)}
+          value={Location}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Manufacturer"
+          onChangeText={(text: string) => setManufacturer(text)}
+          value={Manufacturer}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Quantity"
+          onChangeText={(text: string) => setQuantity(text)}
+          value={Quantity}
+        />
+        <Button onPress={addPart} title="Add Part" />
       </View>
+      <Button
+        title="Go to Inventory"
+        onPress={() => {
+          // Pass the input values to the Inventory screen using params
+          navigation.navigate('Inventory', {
+            PartName,
+            PartNumber,
+            Location,
+            Manufacturer,
+            Quantity,
+          });
+        }}
+      />
     </View>
   );
 };
 
 export default List;
 
-
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
   TopText: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     fontSize: 20,
   },
   input: {
     fontSize: 26,
     padding: 20,
     borderBottomWidth: 1,
-  }
-})
+  },
+});
