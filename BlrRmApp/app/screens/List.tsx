5a56ea70e7acf9208eda92df5bb698efaf646b7f
Misael <maq3009@hotmail.com>
2023-11-01 20:45:49 -0400

    Able to add todo -still working

--- BlrRmApp/app/screens/List.tsx
+++ BlrRmApp/app/screens/List.tsx
@@ -1,29 +1,46 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native'
-import { useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { useState, useEffect } from 'react';
 import { FIRESTORE_DB } from '../../firebaseConfig';
 import { addDoc, collection } from 'firebase/firestore';
 
 
 
 const List = ({ navigation }: any) => {
+  const [todos, setTodos] = useState<any[]>([]);
+  const [todo, setTodo] = useState('');
+  
+  
   useEffect(() => {
   },[]);
 
   const addTodo = async () => {
-    console.log('ADD');
-
-    const doc = addDoc(collection(FIRESTORE_DB, 'todos'), { title: 'I am a test', done: false})
-    console.log(' ~ file: List.tsx:12 ~ addTodo ~ doc:', doc);
+    const doc = await addDoc(collection(FIRESTORE_DB, 'todos'), { title: todo, done: false})
+    setTodo('');
   };
 
 
   return (
-    <View>
-        <Text>List</Text>
+    <View style={styles.container}>
+      <View>
+        <TextInput style={styles.input} placeholder='Add new todo'
+        onChangeText={(text: string) => setTodo(text)}
+        value={todo}
+        />
         <Button onPress={() => addTodo()} title="Add Todo" />
+      </View>
     </View>
   );
 };
 
 export default List;
+
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  input: {
+
+  }
+})
